2516cdfc48415ba5397dc39ca21104405b55c929
Rohit Notna <rohitnotna009@gmail.com>
2023-12-06 23:02:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import FormValidation from './Pages/FormValidation';
+import Table from './Pages/Table';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<FormValidation/>}/>
+       <Route path="/table" element={<Table/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
